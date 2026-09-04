4238c7f801db08fca6f46b6c910a837b8dad37fa
ThandinkosiSibiya <2575660@students.wits.ac.za>
2026-09-04 23:00:40 +0200

    Update server.js
    update app.listen.

--- server.js
+++ server.js
@@ -4142,5 +4142,5 @@ app.get('/api/profile', async (req, res) => {
 // =============================
 
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
